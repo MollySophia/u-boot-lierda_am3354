76f5378798c034192c2bcef0a1cf50b1ded85b23
Pratyush Yadav <p.yadav@ti.com>
2020-08-20 00:16:02 +0000

    mtd: spi-nor-core: Perform a Soft Reset on shutdown
    On probe, the SPI NOR core will put a flash in 8D-8D-8D mode if it
    supports it. But Linux as of now expects to get the flash in 1S-1S-1S
    mode. Handing the flash to Linux in Octal DTR mode means the kernel will
    fail to detect the flash.
    
    So, we need to reset to Power-on-Reset (POR) state before handing off
    the flash. A Software Reset command can be used to do this.
    
    One limitation of the soft reset is that it will restore state from
    non-volatile registers in some flashes. This means that if the flash was
    set to 8D mode in a non-volatile configuration, a soft reset won't help.
    This commit assumes that we don't set any non-volatile bits anywhere,
    and the flash doesn't have any non-volatile Octal DTR mode
    configuration.
    
    Since spi-nor-tiny doesn't (and likely shouldn't) have
    spi_nor_soft_reset(), add a dummy spi_nor_remove() for it that does
    nothing.
    
    Signed-off-by: Pratyush Yadav <p.yadav@ti.com>

--- drivers/mtd/spi/sf_probe.c
+++ drivers/mtd/spi/sf_probe.c
@@ -152,6 +152,13 @@ static int spi_flash_std_probe(struct udevice *dev)
 
 static int spi_flash_std_remove(struct udevice *dev)
 {
+	struct spi_flash *flash = dev_get_uclass_priv(dev);
+	int ret;
+
+	ret = spi_nor_remove(flash);
+	if (ret)
+		return ret;
+
 #if CONFIG_IS_ENABLED(SPI_FLASH_MTD)
 	spi_flash_mtd_unregister();
 #endif
@@ -178,6 +185,7 @@ U_BOOT_DRIVER(spi_flash_std) = {
 	.remove		= spi_flash_std_remove,
 	.priv_auto_alloc_size = sizeof(struct spi_flash),
 	.ops		= &spi_flash_std_ops,
+	.flags		= DM_FLAG_OS_PREPARE,
 };
 
 #endif /* CONFIG_DM_SPI_FLASH */
