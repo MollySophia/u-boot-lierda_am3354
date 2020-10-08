4a541d1e21833cc8d18ec6385df0f7a4b31ab9d2
Peter Ujfalusi <peter.ujfalusi@ti.com>
2020-10-08 21:55:33 +0000

    omap4: sdp: convert to device model
    commit 8812ed97255a30a4f625e3d99a5108bf3e61ec95 upstream.
    
    Convert omap4 sdp to device model.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

--- board/ti/sdp4430/sdp.c
+++ board/ti/sdp4430/sdp.c
@@ -7,6 +7,7 @@
  */
 #include <common.h>
 #include <twl6030.h>
+#include <serial.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/mmc_host_def.h>
 
@@ -88,6 +89,17 @@ void board_mmc_power_init(void)
 }
 #endif
 #endif
+
+#if defined(CONFIG_SPL_OS_BOOT)
+int spl_start_uboot(void)
+{
+	/* break into full u-boot on 'c' */
+	if (serial_tstc() && serial_getc() == 'c')
+		return 1;
+
+	return 0;
+}
+#endif /* CONFIG_SPL_OS_BOOT */
 
 /*
  * get_board_rev() - get board revision
