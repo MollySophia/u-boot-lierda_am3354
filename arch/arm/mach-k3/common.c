aca42fc285ab05fe22a4aa5471c0e67fd5c42f02
Dave Gerlach <d-gerlach@ti.com>
2020-10-09 17:49:53 +0000

    arm: mach-k3: common: Drop main r5 start
    Drop the main R5 startup for now.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- arch/arm/mach-k3/common.c
+++ arch/arm/mach-k3/common.c
@@ -206,7 +206,6 @@ void __noreturn jump_to_image_no_args(struct spl_image_info *spl_image)
 		panic("rproc failed to be initialized (%d)\n", ret);
 
 	init_env();
-	start_non_linux_remote_cores();
 	if (!fit_image_info[IMAGE_ID_DM_FW].image_start)
 		size = load_firmware("mcur5f0_0fwname", "mcur5f0_0loadaddr",
 				     &loadaddr);
