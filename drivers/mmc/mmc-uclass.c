MollySophia/u-boot-lierda_am3354

commit 654d73f8725db340ccb470d5d130df3fcf7ce482

mmc: mmc-uclass: Assign devnum as alias index in SPL too if CONFIG_SPL_DM_SEQ_ALIAS is enabled
Assign the nodes alias index as the device number in SPL too, if
CONFIG_SPL_DM_SEQ_ALIAS is enabled.

Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

drivers/mmc/mmc-uclass.c
@@ -325,11 +325,11 @@ int mmc_bind(struct udevice *dev, struct mmc *mmc, const struct mmc_config *cfg)
 
 	if (!mmc_get_ops(dev))
 		return -ENOSYS;
-#ifndef CONFIG_SPL_BUILD
-	/* Use the fixed index with aliase node's index */
-	ret = dev_read_alias_seq(dev, &devnum);
-	debug("%s: alias ret=%d, devnum=%d\n", __func__, ret, devnum);
-#endif
+	if (IS_ENABLED(CONFIG_SPL_DM_SEQ_ALIAS)) {
+		/* Use the fixed index with aliase node's index */
+		ret = dev_read_alias_seq(dev, &devnum);
+		debug("%s: alias ret=%d, devnum=%d\n", __func__, ret, devnum);
+	}
 
 	ret = blk_create_devicef(dev, "mmc_blk", "blk", IF_TYPE_MMC,
 			devnum, 512, 0, &bdev);

commit 104e195bf238d9df8e95c1521c97b737c8e452dd

mmc: mmc-uclass: Use req_seq field in struct udevice to read aliases node's index
dev_read_alias_seq() needs CONFIG_SPL_OF_LIBFDT to be enabled and on some
boards it is disabled, CONFIG_SPL_DM_SEQ_ALIAS is enabled which leads to
break in build. In order remove this dependency use req_seq field in
struct udevice to read aliases node's  index.

This patch fixes the omapl138-lcdk build failure caused by the below
mentioned commit.

Fixes: 654d73f8725d ("mmc: mmc-uclass: Assign devnum as alias index in SPL too if CONFIG_SPL_DM_SEQ_ALIAS is enabled")
Suggested-by: Grygorii Strashko <grygorii.strashko@ti.com>
Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>
Reviewed-by: Grygorii Strashko <grygorii.strashko@ti.com>

drivers/mmc/mmc-uclass.c
@@ -321,18 +321,16 @@ int mmc_bind(struct udevice *dev, struct mmc *mmc, const struct mmc_config *cfg)
 {
 	struct blk_desc *bdesc;
 	struct udevice *bdev;
-	int ret, devnum = -1;
+	int ret;
 
 	if (!mmc_get_ops(dev))
 		return -ENOSYS;
-	if (IS_ENABLED(CONFIG_SPL_DM_SEQ_ALIAS)) {
-		/* Use the fixed index with aliase node's index */
-		ret = dev_read_alias_seq(dev, &devnum);
-		debug("%s: alias ret=%d, devnum=%d\n", __func__, ret, devnum);
-	}
+
+	/* Use the fixed index with aliases node's index */
+	debug("%s: alias devnum=%d\n", __func__, dev->req_seq);
 
 	ret = blk_create_devicef(dev, "mmc_blk", "blk", IF_TYPE_MMC,
-			devnum, 512, 0, &bdev);
+			dev->req_seq, 512, 0, &bdev);
 	if (ret) {
 		debug("Cannot create block device\n");
 		return ret;