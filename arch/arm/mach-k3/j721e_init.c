MollySophia/u-boot-lierda_am3354

commit 4d486c2ef44dbb49a0922da1706a991cecdb0f4a

arm: mach-k3: j721e_init: Force early probe of clk-k3 driver
Force the clk-k3 driver to probe early during R5 SPL boot to ensure the
default system clock configuration is completed. Many other drivers
assume a default state of the clock tree and it is currently possible
for them to probe before clk-k3 depending on the exact system
configuration.

Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
Reported-by: Keerthy <j-keerthy@ti.com>
Tested-by: Keerthy <j-keerthy@ti.com>

arch/arm/mach-k3/j721e_init.c
@@ -277,6 +277,18 @@ void board_init_f(ulong dummy)
 	/* Init DM early */
 	spl_early_init();
 
+#ifdef CONFIG_SPL_CLK_K3
+	/*
+	 * Force probe of clk_k3 driver here to ensure basic default clock
+	 * configuration is always done.
+	 */
+	ret = uclass_get_device_by_driver(UCLASS_CLK,
+					  DM_GET_DRIVER(ti_clk),
+					  &dev);
+	if (ret)
+		panic("Failed to initialize clk-k3!\n");
+#endif
+
 #ifdef CONFIG_K3_LOAD_SYSFW
 	/*
 	 * Process pinctrl for the serial0 a.k.a. MCU_UART0 module and continue

commit 2781231a33c3d779e32445f4fe55164c45d6d7c1

arm: mach-k3: j721e_init: probe clock node after sci node is probed
Make sure the clock driver is probed after sci driver is up and running.
Else in-case of non-combined image ROM boot, it will fail as DMSC will
not be up.

Fixes: 4d486c2ef44db ("arm: mach-k3: j721e_init: Force early probe of clk-k3 driver")
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
Reported-by: Aswath Govindraju <a-govindraju@ti.com>
Reviewed-by: Dave Gerlach <d-gerlach@ti.com>

arch/arm/mach-k3/j721e_init.c
@@ -277,18 +277,6 @@ void board_init_f(ulong dummy)
 	/* Init DM early */
 	spl_early_init();
 
-#ifdef CONFIG_SPL_CLK_K3
-	/*
-	 * Force probe of clk_k3 driver here to ensure basic default clock
-	 * configuration is always done.
-	 */
-	ret = uclass_get_device_by_driver(UCLASS_CLK,
-					  DM_GET_DRIVER(ti_clk),
-					  &dev);
-	if (ret)
-		panic("Failed to initialize clk-k3!\n");
-#endif
-
 #ifdef CONFIG_K3_LOAD_SYSFW
 	/*
 	 * Process pinctrl for the serial0 a.k.a. MCU_UART0 module and continue
@@ -310,6 +298,18 @@ void board_init_f(ulong dummy)
 	 */
 	k3_sysfw_loader(is_rom_loaded_sysfw(&bootdata),
 			k3_mmc_stop_clock, k3_mmc_restart_clock);
+
+#ifdef CONFIG_SPL_CLK_K3
+	/*
+	 * Force probe of clk_k3 driver here to ensure basic default clock
+	 * configuration is always done.
+	 */
+	ret = uclass_get_device_by_driver(UCLASS_CLK,
+					  DM_GET_DRIVER(ti_clk),
+					  &dev);
+	if (ret)
+		panic("Failed to initialize clk-k3!\n");
+#endif
 
 	/* Prepare console output */
 	preloader_console_init();